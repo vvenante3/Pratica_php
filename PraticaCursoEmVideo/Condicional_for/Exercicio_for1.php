60bbbfdbf876fa46766195bfc6d013a817f80f3b
VitorVenante <vitor.venante@hotmail.com>
2025-10-01 23:35:12 -0300

    condicional for

---
+++ PraticaCursoEmVideo/Condicional_for/Exercicio_for1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 01</title>
+</head>
+<body>
+    <?php
+
+        for($c=0; $c<=10; $c++) {
+            echo "$c ";
+        }
+
+    ?>  
+</body>
+</html>
---
+++ PraticaCursoEmVideo/Condicional_for/condicional_for.php
@@ -0,0 +1,12 @@
+<?php
+
+    // o laço FOR você: 
+    //      1. define o ponto inicial da variável, 
+    //      2. delimita o percurso que será realizado, 
+    //      3. e por fim a forma que esse percurso será realizado
+
+    for($c=1; $c<10; $c++) {
+        echo $c;
+    }
+
+?>
